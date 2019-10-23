1819ed25c6aea3744525903c7738102395f6f925
Agc96 <anthony.gutierrez@pucp.pe>
2019-10-22 20:42:38 -0500

    Avances Tklinter con maptlotlib

--- 09-gui/ejemplo.py
+++ 09-gui/ejemplo.py
@@ -5,48 +5,57 @@ Created on Mon Oct 21 13:51:35 2019
 @author: Agutierrez
 """
 
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-from scipy.constants import g as GRAVEDAD
 import tkinter as tk
 from tkinter.messagebox import showerror
+
+import numpy as np
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.animation import FuncAnimation
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+
+from scipy.constants import g as GRAVEDAD
 
 # Inicializar la ventana
 ventana = tk.Tk()
 ventana.title("Ejemplo de movimiento telúrico")
 ventana.geometry("800x600")
 
+# Inicializar el frame de ingreso de datos
+frame = tk.Frame(ventana)
+frame.pack(side=tk.LEFT)
+
 # Función auxiliar para generar datos de entrada
-def generar_dato_entrada(ventana, titulo, fila):
+def generar_dato_entrada(frame, titulo, fila):
     variable = tk.DoubleVar()
     # Configurar etiqueta para los datos
-    etiqueta = tk.Label(ventana, text=titulo)
+    etiqueta = tk.Label(frame, text=titulo)
     etiqueta.grid(row=fila, column=0, padx=5, pady=5) # place(x=10, y=fila*30 - 20)
     # Configurar entrada para los datos
-    entrada = tk.Entry(ventana, textvariable=variable, justify="right")
+    entrada = tk.Entry(frame, textvariable=variable, justify="right")
     entrada.grid(row=fila, column=1, padx=5, pady=5) # place(x=140, y=fila*30 - 20)
     return variable
 
-def generar_dato_intermedio(ventana, titulo, fila):
+def generar_dato_intermedio(frame, titulo, fila):
     variable = tk.Variable()
     # Configurar etiqueta para los datos
-    etiqueta = tk.Label(ventana, text=titulo)
+    etiqueta = tk.Label(frame, text=titulo)
     etiqueta.grid(row=fila, column=0, padx=5, pady=5)
     # Configurar entrada para los datos
-    entrada = tk.Entry(ventana, textvariable=variable, justify="right",
+    entrada = tk.Entry(frame, textvariable=variable, justify="right",
                        state="disabled")
     entrada.grid(row=fila, column=1, padx=5, pady=5)
     return variable
 
 # Generar datos de entrada
-base_var = generar_dato_entrada(ventana, "Base (m):", 0) # Base (b)
-altura_var = generar_dato_entrada(ventana, "Altura (m):", 1) # Altura (h)
-masa_var = generar_dato_entrada(ventana, "Masa (kg):", 2) # Masa (M)
-radio_var = generar_dato_entrada(ventana, "Radio (m):", 3) # Radio (R)
+base_var = generar_dato_entrada(frame, "Base (m):", 0) # Base (b)
+altura_var = generar_dato_entrada(frame, "Altura (m):", 1) # Altura (h)
+masa_var = generar_dato_entrada(frame, "Masa (kg):", 2) # Masa (M)
+radio_var = generar_dato_entrada(frame, "Radio (m):", 3) # Radio (R)
 # Generar datos intermedios
-alfa_var = generar_dato_intermedio(ventana, "Ángulo alfa:", 4)
-inercia_var = generar_dato_intermedio(ventana, "Inercia (kg*m^2):", 5)
+alfa_var = generar_dato_intermedio(frame, "Ángulo alfa:", 4)
+inercia_var = generar_dato_intermedio(frame, "Inercia (kg*m^2):", 5)
 
 # Función para iniciar la simulación del movimiento telúrico
 def simulacion():
@@ -68,9 +77,18 @@ def simulacion():
                            "Verifique que los datos sean válidos.")
 
 # Botón de iniciar simulación
-btn_iniciar = tk.Button(ventana, text="Iniciar", command=simulacion)
+btn_iniciar = tk.Button(frame, text="Iniciar", command=simulacion)
 btn_iniciar.grid(row=7, columnspan=2) # place(x=120, y=190)
 
+# Generar gráfico principal
+fig = Figure(figsize=(2, 2))
+fig.gca().grid(True)
+canvas = FigureCanvasTkAgg(fig, master=ventana)
+canvas.draw()
+canvas.get_tk_widget().pack(side=tk.RIGHT, fill=tk.X, expand=1)
+
+# Generar gráficos auxiliares
+
 # Interactuar con la ventana
-ventana.mainloop()
+tk.mainloop()
 
